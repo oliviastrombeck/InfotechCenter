d3e9e61053802e7ef2bd35f2acaa57dc7e5637a0
Strombeck <02olivia.strombeck@gulllakecs.org>
2025-01-31 14:15:48 -0500

    Added Libraries & While Loop for Ellipsis - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,3 +1,21 @@
+#Libraries HERE
+import sys
+import time
+
 print("Welcome Branch - Developer: Olivia Strombeck")
 
 print ("\n\tWelcome to InfoTechCenter V1.0")
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("Infotech Center System Booting" + "." * ellipsis)
+    ellipsis += 1
+    sys.stdout.write("\r" + message)
+    time.sleep(1)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
