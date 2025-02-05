fee69f555110a3a7c16dcd68e3bd8fa087c6c2df
Strombeck <02olivia.strombeck@gulllakecs.org>
2025-02-05 14:16:02 -0500

    Added Weather Function - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,15 @@
-# Programmer: Olivia Strombeck
-# Branch: Weather
-# Date:
-# Program:
+print("\n********************************************\n")
+
+print("Weather Branch - Developer: Olivia Strombeck\n")
+
+#Import Libraries Here!
+import random
+from time import sleep
+
+#Weather Function to determine the Weather
+def weather():
+    weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition
+
+print (weather())
