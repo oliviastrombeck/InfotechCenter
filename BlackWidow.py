d9fe45f907a96a7cb11e3b637d4f0b1602ac72da
oliviastrombeck <02olivia.strombeck@gulllakecs.org>
2025-02-17 14:10:02 -0500

    Updated Spacing - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -21,18 +21,18 @@ def gasLevelAlert():
         sleep(1)
         print("Calling AAA")
     elif gasLevelIndicator == "Low":
-        print("Your gas tank is really low, checking GPS for closest gas station.")
+        print("\nYour gas tank is really low, checking GPS for closest gas station.\n")
         sleep(1)
         print("The closest gas station is", gasStations(), "which is", milesToGasStationLow, "miles away.")
     elif gasLevelIndicator == "Quarter Tank":
-        print("Your gas tank is on a Quarter Tank, checking GPS for closest gas station.")
+        print("\nYour gas tank is on a Quarter Tank, checking GPS for closest gas station.\n")
         sleep(1)
         print("The closest gas station is", gasStations(), "which is", milesToGasStationQuarterTank, "miles away.")
     elif gasLevelIndicator == "Half Tank":
-        print("Your gas tank is on a Half of a Tank, which is plenty to get to your destination!")
+        print("\nYour gas tank is on a Half of a Tank, which is plenty to get to your destination!\n")
     elif gasLevelIndicator == "Three Quarter Tank":
-        print("Your gas tank is Three Quarter of a Tank full!")
+        print("\nYour gas tank is Three Quarter of a Tank full!\n")
     else:
-        print("Your gas tank is FULL.")
+        print("\nYour gas tank is FULL.\n")
 
 gasLevelAlert()
