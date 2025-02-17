37e7ac40778b5c59e0b04cd9c28734c4304d7a2d
oliviastrombeck <02olivia.strombeck@gulllakecs.org>
2025-02-17 13:53:42 -0500

    Added GasLevelAlert Function - Created Variables  - Elif through Quarter Tank - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -12,5 +12,21 @@ def gasStations():
     gasStationsList = ["Shell", "Marathon", "Speedway", "Circle K", "Wesco", "Meijer", "Buc-ees", "Costco"]
     return random.choice (gasStationsList)
 
-print(gasLevelGauge())
-print(gasStations())
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(1,25),1)
+    milesToGasStationQuarterTank = round(random.uniform(25.1,50), 1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty":
+        print("******wARNING - YOU ARE OUT OF GAS******\n")
+        sleep(1)
+        print("Calling AAA")
+    elif gasLevelIndicator == "Low":
+        print("Your gas tank is really low, checking GPS for closest gas station.")
+        sleep(1)
+        print("The closest gas station is", gasStations(), "which is", milesToGasStationLow, "miles away.")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("Your gas tank is on a Quarter Tank, checking GPS for closest gas station.")
+        sleep(1)
+        print("The closest gas station is", gasStations(), "which is", milesToGasStationQuarterTank, "miles away.")
+
+gasLevelAlert()
