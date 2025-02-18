5538c720b611276329690731ba57aa5c56eab924
Strombeck <02olivia.strombeck@gulllakecs.org>
2025-02-18 14:00:58 -0500

    Finished VRS() - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -17,12 +17,20 @@ weatherAlert = weather()
 def vehicleResponseSystem():
     if weatherAlert == "snowing":
         print("\nThe National Weather Service has Updated your alarm by 30 minutes because"
-        " it is", weatherAlert, "outside.")
+        " it is", weatherAlert, "outside!")
     elif weatherAlert == "blizzard":
         print("\nThe National Weather Service has updated your alarm by 60 minutes because"
               " it is a", weatherAlert, "outside!")
     elif weatherAlert == "icy":
         print("\nThe National Weather Service has updated your alarm by 90 minutes because"
               " it is", weatherAlert, "outside!")
+    elif weatherAlert == "rainy":
+        print("\nThe National Weather Service has updated your alarm by 10 minutes because"
+              " it is", weatherAlert, "outside!")
+    elif weatherAlert == "windy":
+        print("\nThe National Weather Service has updated your alarm by 5 minutes because"
+              " it is", weatherAlert, "outside!")
+    else:
+        print("\nThe National Weather Service is calling for", weatherAlert, "skys outside, drive safe!")
 
 vehicleResponseSystem()
